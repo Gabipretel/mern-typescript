734f952174f2ccfa3de46cc35d3f648677a68cec
Gabipretel <gabypretel@gmail.com>
2022-12-27 10:45:46 -0300

    add config Model, routes,app

--- api/src/routes/Video.ts
+++ api/src/routes/Video.ts
@@ -1,1 +1,24 @@
-//Modelo Video
+//Modelo Schema Video
+import {Schema,model} from 'mongoose'
+
+const videoSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String,
+        trim:true
+    },
+    url:{
+        type:String,
+        required: true,
+        trim:true
+    }
+
+},{
+    versionKey:false,
+    timestamps:true
+})
+export default model('Video',videoSchema)
