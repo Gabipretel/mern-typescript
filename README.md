e51976809a2017be9bbff89e2bdb5de5d01baec7
Gabipretel <gabypretel@gmail.com>
2023-01-13 22:27:37 -0300

    CRUD terminado

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # MERN-Typescript
 Proyecto de VIDEOS CON EL STACK MERN (MONGO,EXPRESS,REACT,NODE)
-Seguir hora y 8 minutos fatz Frontend Typescript 
+Averiguar porque el hook rompe la app...
--- client/src/components/Videos/VideoForm.tsx
+++ client/src/components/Videos/VideoForm.tsx
@@ -1,32 +1,50 @@
-import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom'
-import { Video } from './Video';
-import * as videoService from './VideoService'
-import {toast} from 'react-toastify'
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { Video } from "./Video";
+import * as videoService from "./VideoService";
+import { toast } from "react-toastify";
+
+interface Params {
+  id?: string;
+}
+type InputChange = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
+type submit = FormEvent<HTMLFormElement>;
 
 const VideoForm = () => {
+  const params = useParams();
+  const navigate = useNavigate();
 
-  const navigate= useNavigate()
-  type InputChange = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
-  type submit = FormEvent<HTMLFormElement>
-
-  const initialState= {
-    title:'',
-    url:'',
-    description:''
-  }
-  const [video,setVideo] = useState<Video>(initialState)
+  const initialState = {
+    title: "",
+    url: "",
+    description: "",
+  };
+  const [video, setVideo] = useState<Video>(initialState);
 
   const handleInputChange = (e: InputChange) =>
     setVideo({ ...video, [e.target.name]: e.target.value });
 
-  const handleSubmit = async (e:submit )=>{
-    e.preventDefault()
-    const res= await videoService.createVideo(video)
-    toast.success('Nuevo video Agregado con Exito😎')
-    setVideo(initialState)
-    navigate('/')
-  }
+  const handleSubmit = async (e: submit) => {
+    e.preventDefault();
+    if (!params.id) {
+      const res = await videoService.createVideo(video);
+      toast.success("Nuevo video Agregado con Exito😎");
+      setVideo(initialState);
+    } else {
+      await videoService.updateVideo(params.id, video);
+    }
+    navigate("/");
+  };
+
+  const getVideo = async (id: string) => {
+    const res = await videoService.getVideoById(id);
+    const { title, description, url } = res.data;
+    setVideo({ title, description, url });
+  };
+
+  useEffect(() => {
+    if (params.id) getVideo(params.id);
+  }, [params.id]);
 
   return (
     <div className="row">
@@ -65,16 +83,15 @@ const VideoForm = () => {
                   className="form-control"
                   placeholder="Escribe una descripción "
                   onChange={handleInputChange}
-                value={video.description}
+                  value={video.description}
                 ></textarea>
               </div>
 
-              {/* {params.id ? (
-                <button className="btn btn-info">Update</button>
+              {params.id ? (
+                <button className="btn btn-info">Actualizar Video</button>
               ) : (
-                <button className="btn btn-primary">Create</button>
-              )} */}
-              <button className="btn btn-outline-success">Crear Video</button>
+                <button className="btn btn-outline-success">Crear Video</button>
+              )}
             </form>
           </div>
         </div>
